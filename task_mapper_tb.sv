5eff5ba905078ac2ff1271d11f68f096d20e2469
rivughoshWB <112155531+rivughoshWB@users.noreply.github.com>
2022-08-29 07:16:53 +0530

    TB

--- task_mapper_tb.sv
+++ task_mapper_tb.sv
@@ -1,5 +1,5 @@
 // Code your testbench here
-// or browse Examples
+
 module array_check_tb; 
   localparam NUM_V=4;   
   reg clk;
@@ -7,40 +7,30 @@ module array_check_tb;
   logic [31:0]task_array; 
   logic root_task;
 
-
+ initial begin
+    clk = 1;  
+    rst = 0;
+  end
 
   initial begin
     #1 root_task=1'b0;   
   end
 
 
-  logic [31:0] task_graph [2:0][2:0];
-  logic [31:0] row ,col;
+  logic [31:0] task_graph [3:0][3:0];
   logic [2:0] count_root_task;
   int i,j;
 
 
-
-  initial begin
-    row=0;
-    col =0;
-  end 
-
-  assign task_graph = '{{32'd0,32'd5,32'd0},{32'd5,32'd0,32'd6},{32'd0,32'd6,32'd0}};
+  assign task_graph = '{{32'd0,32'd0,32'd0,32'd7},{32'd0,32'd0,32'd6,32'd0},{32'd0,32'd6,32'd0,32'd5},{32'd7,32'd0,32'd5,32'd0}};
+  
+  
   //pushing each application loop ~~~~~~~~~~ 
   initial begin
-    row=0;
     count_root_task='0;
     // giving opp. becuase of SV array
-    for(i=2;i>=0;i--) begin
-      col=0;
-      for(j=2;j>=0;j--) begin
-        if( root_task == '0) begin
-          #20; //2 clk cyle to read next task
-        end else begin 
-          #10 root_task=1'b0;
-          #10;
-        end
+    for(i=0;i<4;i++) begin
+      for(j=0;j<4;j++) begin
         task_array = task_graph[i][j];
         if (task_array!='0) begin 
           count_root_task=count_root_task+3'b001;
@@ -48,14 +38,18 @@ module array_check_tb;
             root_task=1'b1;
           end
         end
+        if( root_task == '0) begin
+          #20; //2 clk cyle to read next task
+        end else begin 
+          #10 root_task=1'b0;
+          #10;
+        end
+
         `ifdef debug_help  
-        $display("time = %f ns i= %d  j=%d task_array= %d task_graph = %d",$time,row,col,task_array,task_graph[i][j]);  
+        $display("time = %f ns i= %d  j=%d task_array= %d task_graph = %d",$time,i,j,task_array,task_graph[i][j]);  
         // $display("time %d ns row %d col %d",$time,row,col);
         `endif
-
-        col++;
       end
-      row++;
     end
   end 
   //pushing each application loop ~~~~~~~~~~
@@ -65,13 +59,12 @@ module array_check_tb;
     .clk    (clk),
     .rst_b  (rst),
     .task_array(task_array),
-    .root_task
+    .root_task,
+    .row(i),
+    .col(j)
   ); 
 
-  initial begin
-    clk = 1;  
-    rst = 0;
-  end 
+  
   //reset generation   
   always   
     #1   rst =1;
@@ -92,8 +85,10 @@ module array_check_tb;
     $monitor("%d,\t%b ,\t%b",$time, clk, rst); 
  end 
  */  
+  
+
   initial 
-    #200 $finish; //1000 clk cylce simulation
+    #550 $finish; //1000 clk cylce simulation
 
 
 endmodule
