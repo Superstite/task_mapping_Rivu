bed47ddabe67251a538896b3a01a28c5bebbe11d
rivughoshWB <112155531+rivughoshWB@users.noreply.github.com>
2022-08-31 19:31:08 +0530

    TB

--- task_mapper_tb.sv
+++ task_mapper_tb.sv
@@ -1,6 +1,6 @@
 // Code your testbench here
-
-module task_mapper_tb; 
+// or browse Examples
+module array_check_tb; 
   localparam NUM_V=4;   
   reg clk;
   reg rst;
@@ -19,38 +19,49 @@ module task_mapper_tb;
 
   logic [31:0] task_graph [3:0][3:0];
   logic [2:0] count_root_task;
-  int i,j;
+  int i,j,app_1;
+  logic app_end;
 
-
+//app_1
   assign task_graph = '{{32'd0,32'd0,32'd0,32'd7},{32'd0,32'd0,32'd6,32'd0},{32'd0,32'd6,32'd0,32'd5},{32'd7,32'd0,32'd5,32'd0}};
   
   
   //pushing each application loop ~~~~~~~~~~ 
   initial begin
+    for(app_1=1;app_1<=10;app_1++) begin
+    app_end=1'b0;
     count_root_task='0;
     // giving opp. becuase of SV array
     for(i=0;i<4;i++) begin
       for(j=0;j<4;j++) begin
         task_array = task_graph[i][j];
+         `ifdef debug_help  
+          $display("time = %f ns i= %d  j=%d task_array= %d task_graph = %d",$time,i,j,task_array,task_graph[i][j]);  
+          
+        // $display("time %d ns row %d col %d",$time,row,col);
+        `endif
+        
         if (task_array!='0) begin 
           count_root_task=count_root_task+3'b001;
           if(count_root_task==3'b001) begin
             root_task=1'b1;
           end
         end
+        @(posedge clk);
         if( root_task == '0) begin
-          #20; //2 clk cyle to read next task
+          //#20; //2 clk cyle to read next task
+          @(posedge clk);
+          
         end else begin 
-          #10 root_task=1'b0;
-          #10;
+          @(posedge clk); root_task=1'b0;
         end
 
-        `ifdef debug_help  
-        $display("time = %f ns i= %d  j=%d task_array= %d task_graph = %d",$time,i,j,task_array,task_graph[i][j]);  
-        // $display("time %d ns row %d col %d",$time,row,col);
-        `endif
       end
     end
+      @(posedge clk);app_end=1'b1;
+      @(posedge clk);app_end=1'b0;
+      @(posedge clk);
+    end //app_1 number loop
   end 
   //pushing each application loop ~~~~~~~~~~
 
@@ -61,7 +72,8 @@ module task_mapper_tb;
     .task_array(task_array),
     .root_task,
     .row(i),
-    .col(j)
+    .col(j),
+    .app_end
   ); 
 
   
@@ -88,7 +100,7 @@ module task_mapper_tb;
   
 
   initial 
-    #550 $finish; //1000 clk cylce simulation
+    #5000 $finish; //1000 clk cylce simulation
 
 
 endmodule
