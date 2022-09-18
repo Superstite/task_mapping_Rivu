53dc2ec7b3f2c332565fb54121df48446bb725ad
rivughoshWB <112155531+rivughoshWB@users.noreply.github.com>
2022-09-19 00:59:41 +0530

    Update task_mapper_tb.sv

--- task_mapper_tb.sv
+++ task_mapper_tb.sv
@@ -1,7 +1,7 @@
 // Code your testbench here
 // or browse Examples
 module array_check_tb; 
-  localparam NUM_V=4;   
+  localparam NUM_V=2;   
   reg clk;
   reg rst;
   logic [31:0]task_array; 
@@ -23,20 +23,25 @@ module array_check_tb;
   logic app_end;
 
   //app_1
- assign task_graph = '{{32'd0,32'd0,32'd0,32'd7},{32'd0,32'd0,32'd6,32'd0},{32'd0,32'd6,32'd0,32'd5},{32'd7,32'd0,32'd5,32'd0}};
+ //assign task_graph = '{{32'd0,32'd0,32'd0,32'd7},{32'd0,32'd0,32'd6,32'd0},{32'd0,32'd6,32'd0,32'd5},{32'd7,32'd0,32'd5,32'd0}};
 
-  //app_2 (mtc,stc1,stc2 covered)
-  //assign task_graph = '{{32'd0,32'd0,32'd7},{32'd0,32'd6,32'd0},{32'd6,32'd0,32'd5}};
+//app_2 (mtc,stc1,stc2 covered)
+  //assign task_graph = '{{32'd0,32'd0,32'd7},{32'd0,32'd0,32'd5},{32'd7,32'd5,32'd0}};
 
+  //app_3 (mtc,stc1,stc2,stc3 covered)
+  assign task_graph = '{{32'd0,32'd5},{32'd5,32'd0}};
+  
+  
   //pushing each application loop ~~~~~~~~~~ 
   initial begin
-    @(posedge clk);
+    @(posedge clk); @(posedge clk);
     for(app_1=1;app_1<=500;app_1++) begin
       app_end=1'b0;
       count_root_task='0;
       // giving opp. becuase of SV array
       for(i=0;i<NUM_V;i++) begin
         for(j=0;j<NUM_V;j++)begin
+          
           task_array = task_graph[i][j];
           `ifdef debug_help  
           $display("time = %f ns i= %d  j=%d task_array= %d task_graph = %d",$time,i,j,task_array,task_graph[i][j]);  
@@ -55,11 +60,11 @@ module array_check_tb;
 
           end else begin 
             @(posedge clk); root_task=1'b0;
-          end
-
+          end 
         end
       end
-      @(posedge clk);app_end=1'b1;
+      app_end=1'b1;
+      @(posedge clk);
       @(posedge clk);app_end=1'b0;
       @(posedge clk);
     end //app_1 number loop
@@ -72,8 +77,8 @@ module array_check_tb;
     .rst_b  (rst),
     .task_array(task_array),
     .root_task,
-    .row(i),
-    .col(j),
+    .row(i==NUM_V?NUM_V-1:i),
+    .col(j==NUM_V?NUM_V-1:j),
     .app_end
   ); 
 
